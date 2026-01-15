0512bb07147c8b34d625cf083c093017d139f4b6
FRDesai <fatsdesai2018@gmail.com>
2026-01-15 13:36:12 +0530

    Fixed the logo file path in the Navigation component to use the correct casing for the image filename.

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -59,7 +59,7 @@ export default function Navigation() {
             <Link href="/" className="flex items-center gap-3">
               <div className="w-10 h-10 relative">
                 <Image
-                  src="/logo/Transparent_logo.png"
+                  src="/logo/transparent_logo.png"
                   alt="Restart Fitness"
                   fill
                   className="object-contain"
